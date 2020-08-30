8e47f02ac09439bb162f78fd4060091cf76d7276
CaledonianEH <62163840+CaledonianEH@users.noreply.github.com>
2020-08-29 17:32:51 -0700

    Fixin' my mistakes

--- src/main/java/net/hyskies/main/listener/Join.java
+++ src/main/java/net/hyskies/main/listener/Join.java
@@ -14,28 +14,31 @@ import org.bukkit.inventory.Inventory;
 
 public class Join implements Listener {
     String prefix = Utils.chat(Files.msgs.getString("prefix"));
+
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void join(PlayerJoinEvent e){
+    public void join(PlayerJoinEvent e) {
         Player p = e.getPlayer();
-        if(Files.config.getBoolean("enable-join") == true){
-            if((p.hasPermission(Files.perms.getString("staff")) && Files.config.getBoolean("enable-staff-join") == true)){
-                if(p.hasPermission(Files.perms.getString("join-1"))) {
-                    String msg = PlaceholderAPI.setPlaceholders(p, Files.msgs.getString("join-1").replace("%player%", p.getName()).replace("%prefix%", prefix));
-                    e.setJoinMessage(Utils.chat(msg));
-                }else if(p.hasPermission(Files.perms.getString("join-2"))) {
-                    String msg = PlaceholderAPI.setPlaceholders(p, Files.msgs.getString("join-2").replace("%player%", p.getName()).replace("%prefix%", prefix));
-                    e.setJoinMessage(Utils.chat(msg));
-                }else if(p.hasPermission(Files.perms.getString("join-3"))) {
-                    String msg = PlaceholderAPI.setPlaceholders(p, Files.msgs.getString("join-3").replace("%player%", p.getName()).replace("%prefix%", prefix));
-                    e.setJoinMessage(Utils.chat(msg));
-                }else if(p.hasPermission(Files.perms.getString("join-4"))) {
-                    String msg = PlaceholderAPI.setPlaceholders(p, Files.msgs.getString("join-4").replace("%player%", p.getName()).replace("%prefix%", prefix));
-                    e.setJoinMessage(Utils.chat(msg));
-                }else if(p.hasPermission(Files.perms.getString("join-5"))) {
-                    String msg = PlaceholderAPI.setPlaceholders(p, Files.msgs.getString("join-5").replace("%player%", p.getName()).replace("%prefix%", prefix));
-                    e.setJoinMessage(Utils.chat(msg));
-                }else{return;}
-            }else{return;}
-        }else{return;}
+        if (Files.config.getBoolean("enable-join") == true) {
+            if ((p.hasPermission(Files.perms.getString("staff")) && Files.config.getBoolean("enable-staff-join") == true) || (p.hasPermission(Files.perms.getString("join-1")))) {
+                String msg = PlaceholderAPI.setPlaceholders(p, Files.msgs.getString("join-1").replace("%player%", p.getName()).replace("%prefix%", prefix));
+                e.setJoinMessage(Utils.chat(msg));
+            } else if (p.hasPermission(Files.perms.getString("join-2"))) {
+                String msg = PlaceholderAPI.setPlaceholders(p, Files.msgs.getString("join-2").replace("%player%", p.getName()).replace("%prefix%", prefix));
+                e.setJoinMessage(Utils.chat(msg));
+            } else if (p.hasPermission(Files.perms.getString("join-3"))) {
+                String msg = PlaceholderAPI.setPlaceholders(p, Files.msgs.getString("join-3").replace("%player%", p.getName()).replace("%prefix%", prefix));
+                e.setJoinMessage(Utils.chat(msg));
+            } else if (p.hasPermission(Files.perms.getString("join-4"))) {
+                String msg = PlaceholderAPI.setPlaceholders(p, Files.msgs.getString("join-4").replace("%player%", p.getName()).replace("%prefix%", prefix));
+                e.setJoinMessage(Utils.chat(msg));
+            } else if (p.hasPermission(Files.perms.getString("join-5"))) {
+                String msg = PlaceholderAPI.setPlaceholders(p, Files.msgs.getString("join-5").replace("%player%", p.getName()).replace("%prefix%", prefix));
+                e.setJoinMessage(Utils.chat(msg));
+            } else {
+                return;
+            }
+        } else {
+            return;
+        }
     }
 }
